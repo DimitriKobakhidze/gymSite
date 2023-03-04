5c52d0e3efe64c93f9e5bd5b34d3650412d587b2
DimitriKobakhidze <dimitri.kobakhidze11@gmail.com>
2023-03-04 14:39:28 +0400

    loading

---
+++ my-app/src/Components/UI/LoadingSpinner.js
@@ -0,0 +1,14 @@
+
+import classes from "./LoadingSpinner.module.css"
+
+const LoadingSpinner = () =>{
+    return(
+        <div className={classes["lds-facebook"]}>
+            <div />
+            <div />
+            <div />
+        </div>
+    )
+}
+
+export default LoadingSpinner
--- my-app/src/Pages/About.js
+++ my-app/src/Pages/About.js
@@ -1,19 +1,32 @@
 
+import { useState } from "react"
+
+import classes from "./About.module.css"
+import bg from "../Images/about.jpg"
+import LoadingSpinner from "../Components/UI/LoadingSpinner"
 
 const About = (props) => {
+    const [loading,setLoading] = useState(true)
     return(
-        <div className="about-page" style={{backgroundImage: `url(${props.bg})`}}>
-            <div className="about-text-div">
-            <p>
-                Here you can get information on how to do the exercises correctly, which is critical to progress.
-            </p>
-            <p>
-                Learn which foods are most beneficial and which will help you achieve results.
-            </p>
-            <p>
-                Also nutritional supplements that will speed up your progress and help you both recover faster after exercise as well as get in better shape.
-            </p>
-            </div>
+        <div className="about-page">
+            <img className={classes["bg"]} src={bg} onLoad={() => setLoading(false)} />
+            {loading ?
+                <div className={classes["spinner-wrapper"]}>
+                    <LoadingSpinner />
+                </div>
+            :
+                <div className="about-text-div">
+                <p>
+                    Here you can get information on how to do the exercises correctly, which is critical to progress.
+                </p>
+                <p>
+                    Learn which foods are most beneficial and which will help you achieve results.
+                </p>
+                <p>
+                    Also nutritional supplements that will speed up your progress and help you both recover faster after exercise as well as get in better shape.
+                </p>
+                </div>
+            }
         </div>
     )
 }
